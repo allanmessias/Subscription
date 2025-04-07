c4c4bf3648d067b79a2d3067ae411e3698bb297e
Allan Messias <allanmessiasdecoelho@gmail.com>
2025-04-07 14:21:35 -0300

    Add repositories interfaces, following Clean Architecture.

---
+++ Subscription.Core/Repositories/IEventHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.DTO;
+
+namespace Subscription.Core.Interfaces
+{
+    public interface IEventHistoryRepository
+    {
+        Task AddAsync(EventHistory eventHistory, CancellationToken cancellationToken = default);
+        Task<EventHistory> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        
+    }
+}
