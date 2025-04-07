d9a81dcf2139a5fceefa2a77ca5ad026afb2aa95
Allan Messias <allanmessiasdecoelho@gmail.com>
2025-04-07 17:40:20 -0300

    Adds repositories both Interface and Implementations. SubscriptionRepository done.

--- Subscription.Core/Repositories/ISubscriptionRepository.cs
+++ Subscription.Core/Repositories/ISubscriptionRepository.cs
@@ -2,14 +2,14 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Subscription.Core;
-using SubscriptionModel = Subscription.Core.DTO.Subscription; 
+using SubscriptionModel = Subscription.Core.Domain.Entities.Subscription;
 
 
 namespace Subscription.Core.Interfaces
 {
     public interface ISubscriptionRepository
     {
-        Task UpdateStatusAsync(Guid subscriptionId, string newStatus, CancellationToken cancellationToken = default);
+        Task UpdateStatusAsync(SubscriptionModel subscription, CancellationToken cancellationToken = default);
         Task<SubscriptionModel> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
         
         Task DeleteAsync (Guid subscriptionId, CancellationToken cancellationToken = default);
