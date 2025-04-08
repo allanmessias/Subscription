358daf0855a43243027d84c259211c5ee48133fd
Allan Messias <allanmessiasdecoelho@gmail.com>
2025-04-08 14:56:48 -0300

    Fix typo

--- Subscription.Core/Domain/Entities/User.cs
+++ Subscription.Core/Domain/Entities/User.cs
@@ -6,7 +6,7 @@ namespace Subscription.Core.Domain.Entities
     public class User
     {
         [Key]
-        public int Id {  get; set; }
+        public Guid Id{  get; set; }
         [Required]
         public string FullName {  get; set; }
 
