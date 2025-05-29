1bac2c62ccb3d6e9373d6d337e31f2487c0dc305
Allan Messias <allanmessiasdecoelho@gmail.com>
2025-05-29 12:25:22 -0300

    Adds configuration for persistence, applying migrations

---
+++ Subscription.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubscriptionModel = Subscription.Core.Domain.Entities.Subscription;
+using Subscription.Core.Domain.Entities;
+
+namespace Subscription.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<SubscriptionModel> Subscriptions { get; set; }
+        public DbSet<EventHistory> EventHistories { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasData(new User
+                {
+                    Id = new Guid("8d868b2a-df36-4a0e-bf40-7271c4f014f7"),
+                    FullName = "Admin",
+                    CreatedAt = new DateTime(2024, 04, 09, 12, 0, 0, DateTimeKind.Utc)
+                });
+
+            modelBuilder.Entity<SubscriptionModel>()
+            .Property(s => s.Status)
+            .HasConversion<string>();
+        }
+    }
+
+}
